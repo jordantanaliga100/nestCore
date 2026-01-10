2220d198cc90b068f366f0ca54bcc9f1103238c2
jordantanaliga100 <jordantanaliga100@gmail.com>
2026-01-10 13:29:49 +0800

    hotfix: skips test

--- src/modules/users/controllers/users.controller.spec.ts
+++ src/modules/users/controllers/users.controller.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../providers/users.service';
 import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockUsersService = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [{ provide: UsersService, useValue: mockUsersService }],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
