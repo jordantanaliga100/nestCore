e191b06c3997aa852fdfe105d24ef27e6e3123fb
jordantanaliga100 <jordantanaliga100@gmail.com>
2026-01-10 21:00:49 +0800

    chore: created tags entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './modules/auth/auth.module';
 import { PostsModule } from './modules/posts/posts.module';
 import { User } from './modules/users/user.entity';
 import { UsersModule } from './modules/users/users.module';
+import { TagsModule } from './modules/tags/tags.module';
 // Modules
 @Module({
   imports: [
@@ -67,6 +68,7 @@ import { UsersModule } from './modules/users/users.module';
     UsersModule,
     PostsModule,
     AuthModule,
+    TagsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/modules/tags/tags.entity.ts
@@ -0,0 +1,57 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Tags {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 256,
+    nullable: false,
+    unique: true,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    length: 512,
+    nullable: false,
+    unique: true,
+  })
+  slug: string;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description?: string;
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  schema?: string;
+
+  @Column({
+    type: 'varchar',
+    length: 2014,
+    nullable: true,
+  })
+  featuredImageUrl?: string;
+
+  @CreateDateColumn()
+  createDate: Date;
+
+  @UpdateDateColumn()
+  updateDate: Date;
+
+  @DeleteDateColumn()
+  deleteDate: Date;
+}
