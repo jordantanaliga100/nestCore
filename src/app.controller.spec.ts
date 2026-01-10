1ac84f86ea05c4551003d7647cc6833c67414cdc
jordantanaliga100 <jordantanaliga100@gmail.com>
2026-01-10 13:22:27 +0800

    changed app controller spec

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('HHello World! 🚀🚀🚀');
     });
   });
 });
