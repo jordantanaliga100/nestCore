dd8f3f5b57ac0baf0d2a40d39980f82c579976ca
jordantanaliga100 <jordantanaliga100>
2026-01-14 21:07:01 +0800

    feat: make a namespace for config

---
+++ src/config/db.config.ts
@@ -0,0 +1,11 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '3000'),
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  name: process.env.POSTGRES_DB,
+  synchronize: process.env.DATABASE_SYNC === 'true' ? true : false,
+  autoLoadEntities: process.env.DATABASE_AUTOLOAD === 'true' ? true : false,
+}));
